54acd69a4aec7492ff25f67ce234b78d620ba016
achatt <alex_chatt@hotmail.co.uk>
2020-12-11 10:32:37 +0000

    challenge 10 part 2 done

--- AOC-Challenge10/AOC-Challenge10.cpp
+++ AOC-Challenge10/AOC-Challenge10.cpp
@@ -5,22 +5,29 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 std::vector<uint16_t> GetNumbersFromFile(std::string FileName);
 
-uint16_t GetMax(std::vector<uint16_t> adapterLits);
 uint32_t Part1JoltJumpMuliplication(std::vector<uint16_t> joltageRatings);
+uint64_t Part2PathCount(int num, std::vector<uint16_t> joltageRatings, std::map<uint16_t, uint64_t> &ExploredPaths);
 
 int main()
 {
 	std::vector<uint16_t> joltageRatings = GetNumbersFromFile("input.txt");
     joltageRatings.push_back(0);
     std::sort(joltageRatings.begin(), joltageRatings.end());
+    joltageRatings.push_back(joltageRatings[joltageRatings.size() -1] + 3);
 
     uint32_t JumpMuliplicationP1 = Part1JoltJumpMuliplication(joltageRatings);
 
     std::cout << "P1: one jumps * three jumps = " << JumpMuliplicationP1 << std::endl;
+
+    std::map<uint16_t, uint64_t> ExploredPaths;
+    uint64_t TotalCombos = Part2PathCount(0, joltageRatings, ExploredPaths);
+
+    std::cout << "Total Combos = " << TotalCombos << std::endl;
 
 	return 0;
 }
@@ -47,25 +54,12 @@ std::vector<uint16_t> GetNumbersFromFile(std::string FileName)
 
 }
 
-uint16_t GetMax(std::vector<uint16_t> adapterLits)
-{
-    uint16_t max = 0;
-
-    for (int i = 0; i < adapterLits.size(); i++)
-    {
-        max = std::max(max, adapterLits[i]);
-    }
-
-    return max + 3;
-}
 
 uint32_t Part1JoltJumpMuliplication(std::vector<uint16_t> joltageRatings)
 {
     uint16_t StartingJolt = 0;
     uint16_t MaxRange = 3;
     uint16_t OneJump = 0, ThreeJump = 0;
-
-    joltageRatings.push_back(GetMax(joltageRatings));
 
     bool UnSolvable = false;
 
@@ -99,3 +93,29 @@ uint32_t Part1JoltJumpMuliplication(std::vector<uint16_t> joltageRatings)
     return OneJump * ThreeJump;
 }
 
+
+uint64_t Part2PathCount(int num, std::vector<uint16_t> joltageRatings, std::map<uint16_t, uint64_t>& ExploredPaths)
+{
+    if (num == joltageRatings.size() - 1)
+    {
+        return 1;
+    }
+    if (ExploredPaths.count(num))
+    {
+        return ExploredPaths[num];
+    }
+
+    uint64_t Answer = 0;
+
+    for (int i = num + 1; i < joltageRatings.size(); i++)
+    {
+        if (joltageRatings[i] - joltageRatings[num] <= 3)
+        {
+            Answer += Part2PathCount(i, joltageRatings, ExploredPaths);
+        }
+    }
+
+    ExploredPaths[num] = Answer;
+
+    return Answer;
+}
